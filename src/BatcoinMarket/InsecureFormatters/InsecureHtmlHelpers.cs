e214c9f77fb2d7f535286d679668e8dae127ce40
skazantsev <sergey.kazantsev1@gmail.com>
2015-08-02 15:14:07 +0300

    Minor changes for DEMO

--- src/BatcoinMarket/InsecureFormatters/InsecureHtmlHelpers.cs
+++ src/BatcoinMarket/InsecureFormatters/InsecureHtmlHelpers.cs
@@ -8,7 +8,7 @@ namespace BatcoinMarket.InsecureFormatters
         public static MvcHtmlString FormatUserName(this HtmlHelper html, Account account)
         {
             var tagBuilder = new TagBuilder("li");
-            tagBuilder.SetInnerText(string.Format("{0} - {1}", account.Username, account.Balance));
+            tagBuilder.InnerHtml = string.Format("{0} - {1}", account.Username, account.Balance);
             return MvcHtmlString.Create(tagBuilder.ToString());
         }
     }
